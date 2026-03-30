2389d99355ce1f2cf4a8318739180763bc0deaf6
Efraim Tenorio <efratec20.tenorio@gmail.com>
2026-03-30 09:16:40 -0300

    Desafio: Mais eventos de domínio para Product

--- src/main/java/com/algaworks/algashop/product/catalog/application/category/event/CategoryUpdatedEvent.java
+++ src/main/java/com/algaworks/algashop/product/catalog/application/category/event/CategoryUpdatedEvent.java
@@ -3,7 +3,6 @@ package com.algaworks.algashop.product.catalog.application.category.event;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.UUID;
 
