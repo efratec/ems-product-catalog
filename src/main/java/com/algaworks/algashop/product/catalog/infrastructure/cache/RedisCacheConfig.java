9f7c5f7ebc6ada5dc0f3f1b762d26eba8ca3f62a
Efraim Tenorio <efratec20.tenorio@gmail.com>
2026-07-19 15:13:07 -0300

    Desafio: Implemente Health check em Ordering e Billing

--- src/main/java/com/algaworks/algashop/product/catalog/infrastructure/cache/RedisCacheConfig.java
+++ src/main/java/com/algaworks/algashop/product/catalog/infrastructure/cache/RedisCacheConfig.java
@@ -1,8 +1,12 @@
 package com.algaworks.algashop.product.catalog.infrastructure.cache;
 
+import org.jspecify.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.cache.autoconfigure.RedisCacheManagerBuilderCustomizer;
+import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -12,7 +16,10 @@ import java.time.Duration;
 @Configuration
 @EnableCaching
 @ConditionalOnProperty(name = "spring.cache.type", havingValue = "redis")
-public class RedisCacheConfig {
+public class RedisCacheConfig implements CachingConfigurer {
+
+    @Autowired
+    private ResilienceCacheErrorHandler resilienceCacheErrorHandler;
 
     @Bean
     public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
@@ -22,6 +29,11 @@ public class RedisCacheConfig {
         return (builder) -> builder.cacheDefaults(defaultCacheConfig)
                 .withCacheConfiguration("algashop:products:v1",
                         defaultCacheConfig.disableCachingNullValues().entryTtl(Duration.ofMinutes(5)));
+    }
+
+    @Override
+    public @Nullable CacheErrorHandler errorHandler() {
+        return resilienceCacheErrorHandler;
     }
 
 }
